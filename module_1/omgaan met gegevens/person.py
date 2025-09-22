4ef3aee44089c0479b4406d58fa53f7dcc2a81fc
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-09-22 11:43:30 +0200

    Create person.py

---
+++ module_1/omgaan met gegevens/person.py
@@ -0,0 +1,12 @@
+naam = input('Wat is je naam? ')
+adres = input('Wat is je woonadres? ')
+postcode = input('Wat is je postcode? ')
+woonplaats = input('Waar woon je? ')
+
+
+print('-----------------------------------')
+print(f'Naam:        {naam}')
+print(f'Adres:       {adres}')
+print(f'Postcode:    {postcode} ')
+print (f'Woonplaats: {woonplaats}')
+print('-----------------------------------')

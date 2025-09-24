d8be32c7d36c4552b159f66d2de0e9ba2a6491a6
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-09-24 15:07:54 +0200

    else-statement

--- module_1/Wat als/meten-is-weten.py
+++ module_1/Wat als/meten-is-weten.py
@@ -5,3 +5,7 @@ if nr1 == nr2:
     print (f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})')
 elif nr1 > nr2:
     print (f'nr1 ({nr1}) is groter dan nr2 ({nr2})')
+else: nr1 < nr2
+print (f'nr2 ({nr2}) is groter dan nr1 ({nr1}))')
+
+

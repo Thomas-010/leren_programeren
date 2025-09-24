abc7b5bdd0c2aa228cd4d2bc12732a74f7f4cd14
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-09-24 13:13:30 +0200

    Create question.py

---
+++ module_1/omgaan met gegevens/question.py
@@ -0,0 +1,7 @@
+naam = input('Hallo, wat is je naam?:')
+leeftijd = input(f'Goedendag {naam}, hoe oud ben je?:')
+favoeten = input(f'En als {leeftijd} jarige, wat eet je het liefst?:')
+drink = input(f'En wat drink je het liefst bij {favoeten}?:')
+
+
+eind = input(f'De {leeftijd} jarige {naam} drinkt het liefst {drink} bij zijn {favoeten}')

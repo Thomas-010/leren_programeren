441513893507b6ba69b34b34e42f55447a9c5aba
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-09-25 14:07:26 +0200

    snackautomaat

---
+++ module_1/Wat als/snackautomaat.py
@@ -0,0 +1,26 @@
+snacks = {
+    'a': 'Chocoladereep',
+    'b': 'Zakje chips',
+    'c': 'Blikje cola',
+    'd': 'Pakje kauwgom',
+    'e': 'Flesje water',
+}
+
+print (snacks)
+gekozensnack = input('Welke snack kies je? [a,b,c,d,e]:')
+if 'a' in gekozensnack:
+    print ('Je krijgt een lekkere chocoladereep!')
+if 'b' in gekozensnack:
+    print ('Je krijgt een klein zakje chips!')
+if 'c' in gekozensnack:
+    print ('Je krijgt een koud blikje cola!')
+if 'd' in gekozensnack:
+    print ('Je krijgt een lekker pakje kauwgom!')
+if 'e' in gekozensnack:
+    print ('Je krijgt een koud flesje water!')
+
+print ('Veel plezier met je gekozen producten!')
+    
+
+
+

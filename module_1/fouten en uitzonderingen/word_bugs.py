69ed7af845244518ba0ec45883498581ccdd2a4c
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-09-26 13:52:16 +0200

    lower bug

---
+++ module_1/fouten en uitzonderingen/word_bugs.py
@@ -0,0 +1,12 @@
+a = (input('Geef een woord: '))
+b = (input('Geef nog een woord: '))
+
+a = a.lower()
+b = b.lower()
+
+if a < b:     
+    print ('Woord 1 heeft meer letters dan woord 2')
+elif a > b:   
+    print ('Woord 1 heeft minder letters dan woord 2')
+else:                   
+    print ('Woord 1 en woord 2 hebben evenveel letters')

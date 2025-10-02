ff8993db0a5331d93423e2811d6d609b35ca00d2
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-10-02 14:16:30 +0200

    pizzacalculator 2.0

--- module_1/Deel 4/De pizzacalculator 2.0.py
+++ module_1/Deel 4/De pizzacalculator 2.0.py
@@ -2,18 +2,31 @@ prijs1 = 6.50
 prijs2 = 9.30
 prijs3 = 11.10
 
+try: 
+    pizza1 = int(input("Hoeveel small pizza's wilt u?:"))
+except ValueError:
+    print("Dit is geen heel nummer!")
+    pizza1 = 0
 
-pizza1 = (input("Hoeveel small pizza's wilt u?: "))
-if not type(pizza1) is int:
-    print ('Dit is geen heel nummer!')
-pizza2 = (input("Hoeveel medium pizza's wilt u?: "))
-if not type(pizza2) is int:
-    print ('Dit is geen heel nummer!')
-pizza3 = (input("Hoeveel large pizza's wilt u?: "))
+try:
+    pizza2 = int(input("Hoeveel medium pizza's wilt u?: "))
+except ValueError:
+    print("Dit is geen heel nummer!")
+    pizza2 = 0
+
+try:
+    pizza3 = int(input("Hoeveel large pizza's wilt u?: "))
+except ValueError:
+    print("Dit is geen heel nummer! ")
+    pizza3 = 0 
 
 print ('**************Kassa bon**************')
-print (f"Pizza's small:\t  {pizza1} x 6.50 =   {prijs1 * pizza1:.2f}")
-print (f"Pizza's medium:\t  {pizza2} x 9.30 =  {prijs2 * pizza2} ")
-print (f"Pizza's large:\t  {pizza3} x 11.10 = {prijs3 * pizza3} ")
+if pizza1 > 0:
+    print (f"Pizza's small:\t  {pizza1} x 6.50 =   {prijs1 * pizza1:.2f}")
+if pizza2 > 0:
+    print (f"Pizza's medium:\t  {pizza2} x 9.30 =  {prijs2 * pizza2:.2} ")
+if pizza3 > 0:  
+    print (f"Pizza's large:\t  {pizza3} x 11.10 = {prijs3 * pizza3:.2f} ")
 print ('--------------------------------------')
-print (f'Te betalen:                     {prijs1 * pizza1 + prijs2 * pizza2 + prijs3 * pizza3} ')
+print (f'Te betalen:                   {prijs1 * pizza1 + prijs2 * pizza2 + prijs3 * pizza3:.2f} ')
+

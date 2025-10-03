dab0078c40acba9714c2fdf126cb0984c7ce75f3
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-10-03 13:14:12 +0200

    min en max

--- module_1/Deel 4/meten-is-weten2.0.py
+++ module_1/Deel 4/meten-is-weten2.0.py
@@ -1,8 +1,9 @@
 nr1 = input('Wat is het 1ste getal?:')
 nr2 = input('Wat is het 2de getal?:')
 
-minimum = 0
-maximum = 1000
+minimum = min(nr1, nr2)
+maximum = max(nr1, nr2)
+
 
 if nr1 == nr2:
     print (f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})')
@@ -11,5 +12,5 @@ elif nr1 > nr2:
 else:
     print (f'nr2 ({nr2}) is groter dan nr1 ({nr1}))')
 
-
 print (f'Het minimum is: {minimum} en het maximimum is: {maximum}')
+

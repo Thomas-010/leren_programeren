1bc6bb98bb73355ea701d2e629b985e54c7bb772
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-10-03 13:21:33 +0200

    Verschil

--- module_1/Deel 4/meten-is-weten2.0.py
+++ module_1/Deel 4/meten-is-weten2.0.py
@@ -1,8 +1,13 @@
-nr1 = input('Wat is het 1ste getal?:')
-nr2 = input('Wat is het 2de getal?:')
+nr1 = int(input('Wat is het 1ste getal?:'))
+nr2 = int(input('Wat is het 2de getal?:'))
 
 minimum = min(nr1, nr2)
 maximum = max(nr1, nr2)
+
+verschil = abs(nr1 - nr2)
+
+nr1 = int(nr1)
+nr2 = int(nr2)
 
 
 if nr1 == nr2:
@@ -11,6 +16,9 @@ elif nr1 > nr2:
     print (f'nr1 ({nr1}) is groter dan nr2 ({nr2})')
 else:
     print (f'nr2 ({nr2}) is groter dan nr1 ({nr1}))')
+
+if verschil > 0:
+    print (f'Het verschil tussen {nr1} en {nr2} is: {verschil}')
 
 print (f'Het minimum is: {minimum} en het maximimum is: {maximum}')
 

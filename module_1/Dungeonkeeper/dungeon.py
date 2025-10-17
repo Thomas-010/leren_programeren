e62601be93340611eb7927aa504d7cf3c16894ba
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-10-17 11:53:07 +0200

    Dungeon update M1.P.O1

--- module_1/Dungeonkeeper/dungeon.py
+++ module_1/Dungeonkeeper/dungeon.py
@@ -4,6 +4,8 @@ import random
 player_attack = 1
 player_defense = 0
 player_health = 3
+new_player_health = 0
+
 
 r1 = random.randint(10,25)
 r2 = random.randint(-5,75)
@@ -17,6 +19,7 @@ print('Je opent de deur.')
 time.sleep(1)
 
 # === [kamer 2] === #
+
 print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
 print('Het standbeeld heeft een sleutel vast.')
 print('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
@@ -31,18 +34,17 @@ elif r3 == "-":
     goed = r1 - r2
 
 if antwoord == goed:
-    print('Het stadbeeld laat de sleutel vallen en je pakt het op')
+    print(f'Het stadbeeld laat de sleutel vallen en je pakt het op')
+    sleutel = True
 else:
     print('Er gebeurt niets....')
+    sleutel = False
 
-print('Je zie een deur achter het standbeeld.')
+print('Je ziet een deur achter het standbeeld.')
 print('')
 time.sleep(1)
 
-# === [kamer 3] === #w
-#item = 'schild'
-#player_defense += 1
-
+# === [kamer 3] === #
 item = random.choice(["Zwaard", "Schild"])
 if item == "Zwaard":
     player_attack += 2
@@ -64,18 +66,20 @@ print(f'Dapper met je nieuwe {item} loop je de kamer binnen.')
 print('Je loopt tegen een zombie aan.')
 
 zombie_hit_damage = (zombie_attack - player_defense)
+new_player_health = (zombie_attack - player_health)
 if zombie_hit_damage <= 0:
     print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
 else:
     zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
-    
     player_hit_damage = (player_attack - zombie_defense)
     player_attack_amount = math.ceil(zombie_health / player_hit_damage)
 
     if player_attack_amount < zombie_attack_amount:
+        damage = (player_attack_amount * zombie_hit_damage)
+        new_player_health = max(0, player_health - damage)
         print(f'In {player_attack_amount} rondes versla je de zombie.')
-        print(f'Je health is nu {player_health}.')
-    else:
+        print(f'Je health is nu {new_player_health}.')
+    elif player_attack_amount > zombie_attack_amount:
         print('Helaas is de zombie te sterk voor je.')
         print('Game over.')
         exit()
@@ -86,3 +90,9 @@ time.sleep(1)
 print('Voorzichtig open je de deur, je wilt niet nog een zombie tegenkomen.')
 print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
 print('Je loopt er naartoe.')
+if sleutel == True:
+    print('Je opent de schatkist met de sleutel!')
+    print('Je heb de dungeon overleefd en gewonnen')
+else:
+    print('Je kan de schatkist niet openen je heb verloren')
+

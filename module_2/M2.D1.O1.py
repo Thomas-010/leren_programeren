74560b9d9936c127b314cd37aa7501166ce89979
Thomas-010 <Thomas.rietveld@ziggo.nl>
2025-12-17 13:08:50 +0100

    M2.D1.O1 - Max 5 pogingen

---
+++ module_2/M2.D1.O1.py
@@ -0,0 +1,14 @@
+wachtwoord = "school"
+poging = 0
+max_aantal = 5
+juist_wachtwoord = True
+
+while poging < max_aantal:
+    invoer = input("Voet het wachtwoord in: ")
+    poging += 1
+    
+    if invoer == wachtwoord:
+        print(f"Je heb het wachtwoord juist geraden {poging} poging(en)! ")
+        break
+else:
+    print('Je mag niet meer inloggen ')

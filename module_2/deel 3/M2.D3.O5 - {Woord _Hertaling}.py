3177847cd7467e534afe71a459e17729c37674e1
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-01-15 14:04:36 +0100

    M2.D3.O5 - {Woord : Hertaling}

---
+++ module_2/deel 3/M2.D3.O5 - {Woord _Hertaling}.py
@@ -0,0 +1,33 @@
+vertaal_woorden = {
+    "kat": "hond",
+    "muur": "hek",
+    "regen": "zonneschijn",
+    "boek": "tablet",
+    "stoel": "bank",
+    "raam": "deur",
+    "lamp": "fakkel"
+}
+
+
+def vertaal(tekst):
+    woorden = tekst.split()
+    nieuwwoord = []
+    for woord in woorden:
+        eerst = woord.strip(".,?!")
+        if eerst in vertaal_woorden:
+            nieuw = vertaal_woorden[eerst]
+            nieuw += woord[len(eerst): ]
+            nieuwwoord.append(nieuw)
+        else:
+            nieuwwoord.append(woord)
+    return " ".join(nieuwwoord)
+
+tekst = input ("Wat voor verhaaltje wil je vertalen:\n")
+
+
+output = vertaal(tekst)
+print("\nVertaald")
+print(output)
+
+
+

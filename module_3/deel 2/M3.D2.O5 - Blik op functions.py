b394ee26b20f9b0b9e997c773e041ffaa390069e
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-03-05 11:25:25 +0100

    M3.D2.O5 - Blik op functions.py

---
+++ module_3/deel 2/M3.D2.O5 - Blik op functions.py
@@ -0,0 +1,12 @@
+import math
+
+def calculate_cilinder_content(height: float, diameter: float) -> float:
+    radius = diameter / 2
+    content = radius * radius * math.pi * height
+    return round(content, 1)
+
+print(calculate_cilinder_content(5.0, 8.0))  
+print(calculate_cilinder_content(7.0, 11.0))  
+print(calculate_cilinder_content(7.0, 18.0)) 
+print(calculate_cilinder_content(2.0, 15.0)) 
+print(calculate_cilinder_content(6.0, 0.0))   

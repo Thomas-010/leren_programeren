6c11fc841b733185f5df9ce014192cc1c9853373
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-03-05 09:21:57 +0100

    added meten is weten function

--- module_1/Deel 2/meten-is-weten.py
+++ module_1/Deel 2/meten-is-weten.py
@@ -1,11 +1,9 @@
-nr1 = input('Wat is het 1ste getal?:')
-nr2 = input('Wat is het 2de getal?:')
+def vergelijk_getallen(nr1: int, nr2: int) -> str:
+    if nr1 == nr2:
+        return f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})'
+    elif nr1 > nr2:
+        return f'nr1 ({nr1}) is groter dan nr2 ({nr2})'
+    else:
+        return f'nr2 ({nr2}) is groter dan nr1 ({nr1})'
 
-if nr1 == nr2:
-    print (f'Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})')
-elif nr1 > nr2:
-    print (f'nr1 ({nr1}) is groter dan nr2 ({nr2})')
-else:
-    print (f'nr2 ({nr2}) is groter dan nr1 ({nr1}))')
-
-
+print(vergelijk_getallen(5, 10))

4787ab1e1b0c1c0ca56507126f3ac8733a200c07
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-04-09 10:16:02 +0200

    M3.D3.O4 - Realiseer

--- module_3/deel 3/raadeens/application.py
+++ module_3/deel 3/raadeens/application.py
@@ -2,7 +2,29 @@ from functions import *
 
 # De flow van je programma
 def main():
-    pass
+    score = 0
+    ronde = 1
+    geheim = geef_willekeurig_getal(1, 1000)
+
+    geraden = False
+    beurt = 1
+
+    while not geraden:
+        gok = vraag_gok_aan_speler(ronde, beurt)
+
+        geraden = controleer_gok(gok, geheim)
+
+
+        score = update_score(score, geraden)
+        
+        print (geef_richtingshint(gok, geheim))
+        
+        temp = geef_temperatuurhint(gok, geheim)
+        if temp != '':
+            print(temp)
+
+    score = update_score(score, geraden)
+    toon_rondescore(ronde, score)
 
 # voer de flow uit
 main()
--- module_3/deel 3/raadeens/functions.py
+++ module_3/deel 3/raadeens/functions.py
@@ -1,45 +1,66 @@
 #defineer hieronder je functies
 
+import random
+
+
 def geef_willekeurig_getal(eerste: int, laatste: int) -> int:
     "Geeft een willekeurig getal tussen eerste en laatste terug."
-    pass
+    return random.randint(eerste, laatste)
 
 def vraag_gok_aan_speler(ronde: int, beurt: int) -> int:
     "Vraagt de speler om een gok te doen en geeft deze terug."
     "Parameters: ronde = huidige ronde, beurt = huidige beurt"
-    pass
+    return int(input(f'Ronde {ronde}, Beurt {beurt}: Voer je gok in: '))
 
 def controleer_gok(gok: int, geheim: int) -> bool:
     "Controleert of de gok gelijk is aan het geheime getal."
     "Geeft true terug als correct anders false"
+    return gok == geheim
     pass
 
 def geef_richtingshint(gok: int, geheim: int) -> str:
     "Geeft een hint of de gok te hoog, te laag of correct is."
-    pass
+    if gok == geheim:
+        return 'Geraden'
+    elif gok < geheim:
+        return 'Hoger'
+    else:
+        return 'Lager'
 
 def geef_temperatuurhint(gok: int, geheim: int) -> str:
+    verschil = abs(gok - geheim)
     "Geeft een hint of de gok warm, koud of heet is."
-    pass
+    
+    if verschil < 20:
+        return 'Je bent heel warm'
+    if verschil < 50:
+        return 'Je bent warm'
+    else: 
+        return '' 
 
 def update_score(score: int, geraden: bool) -> int:
     "Update de score van de speler op basis van de huidige ronde."
     "Voegt 1 punt toe als de speler correct heeft geraden, anders geen punten"
-    pass
+    if geraden:
+        return score + 1
+    return score
+    
 
 def toon_rondescore(ronde: int, score: int):
     "Toont de huidige ronde en de score van de speler."
     "Parameters: score = punten tot nu toe, ronde = gespeelde rondes"
-    pass
+    print(f'Ronde {ronde} is afgelopen. Je score is: {score}')
 
 def toon_eindscore(score: int):
     "Toont de eindscore van de speler na alle rondes."
-    pass
+    print(f'Je eindscore is: {score}')
+
 
 def vraag_nog_een_keer() -> bool:
     "Vraagt de speler of hij nog een keer wil spelen."
     "Geeft true terug als de speler nog een keer wil spelen, anders false"
-    pass
+    antwoord = input('Wil je nog een keer spelen? (ja/nee): ')
+    return antwoord.lower() == 'ja'
 
 
 #defineer hierboven je functies

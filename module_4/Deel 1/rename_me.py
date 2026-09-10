26a5a1b30725e4024f2a1b0a0f42ebf4df472530
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-09-10 14:08:17 +0200

    M4.D1.O1 rename_me

--- module_4/Deel 1/rename_me.py
+++ module_4/Deel 1/rename_me.py
@@ -1,28 +1,44 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool:
-    return stellar_broccoli % 2 == 0
+def is_even(getal:int) -> bool:
+    return getal % 2 == 0
 
-def chaos_papegaai(fantasie_platypus:str) -> str:
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
-    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
-    return tijdmachine_pannenkoekenmix
+#print(is_even(4))
+#Kijkt of het getal even is
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int:
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float:
-    wobbelwobbel = planetair_taartje.split()
+def draait_woorden_om(tekst:str) -> str:
+    woorden = tekst.split()
+    omgedraaide_woorden = woorden[::-1]
+    omgekeerde_zin = ' '.join(omgedraaide_woorden)
+    return omgekeerde_zin
+
+#print(draait_woorden_om("Hallo ik ben een papegaai"))
+#Dit draait de worden om die je invoert
+
+def tel_unieke_letter(tekst:str) -> int:
+    unieke_tekens = set(tekst)
+    aantal_unieke_tekens = len(unieke_tekens)
+    return aantal_unieke_tekens
+
+#print(tel_unieke_letter("Hallo ik ben een papegaai"))
+#Telt de unieke letters in de zin die ik invoer
+
+def ber_gemiddelde_woordlenge(zin:str) -> float:
+    woorden = zin.split()
     
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+    totaal_aant_tekens = 0
+    for snorkelwagen in woorden:
+        totaal_aant_tekens += len(snorkelwagen)
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    gemiddelde_lengte = totaal_aant_tekens / len(woorden)
+    return gemiddelde_lengte
 
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None:
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
+#print(ber_gemiddelde_woordlenge("Hallo ik ben een papegaai"))
+# Dit berekent het gemiddelde van de zin die je invoert
+
+def vermenigvuldigingstafel(getal:int, aantal:int=10) -> None:
+    for vermenigvuldiger in range(1, aantal+1):
+        resultlaat = vermenigvuldiger * getal
+        print(f'{vermenigvuldiger} x {getal} = {resultlaat}')
+
+#print(vermenigvuldigingstafel(5, 10))
+#Dit print de ingevulde tafel van het getal wat ingevoerd wordt

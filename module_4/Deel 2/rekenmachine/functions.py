f1c56d4bda553bf6de638297f6ffb790bf74f5c3
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-09-18 14:01:56 +0200

    M4.D2.O1

---
+++ module_4/Deel 2/rekenmachine/functions.py
@@ -0,0 +1,15 @@
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+    
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+
+def division(number1, number2):
+    return number1 / number2

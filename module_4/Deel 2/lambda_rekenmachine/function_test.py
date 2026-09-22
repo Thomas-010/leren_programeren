46be05c15f9d3095883bc39850146231d3851656
Thomas-010 <Thomas.rietveld@ziggo.nl>
2026-09-22 09:43:50 +0200

    Commit

---
+++ module_4/Deel 2/lambda_rekenmachine/function_test.py
@@ -0,0 +1,19 @@
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtraction(number1, number2):
+    return number1 - number2
+
+    
+
+def multiplication(number1, number2):
+    return number1 * number2
+
+
+def division(number1, number2):
+    return number1 / number2
+
+
+
+CALCULATIONS = {}
